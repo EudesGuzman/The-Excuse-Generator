e049dc509cd8cd36e75d1a4b753f411c7f06d17d
Eudes Guzmán <eudesgg19@gmail.com>
2020-07-04 14:37:26 +0000

    Ejercicio terminado

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 window.onload = function (){
 
-let who = ['Un elefante','Devian','Mi compañero de clase','Mi ordenador'];
-let what = ['pisoteó','comió','reventó','escupió'];         
-let where =['en un helicoptero','en un lago', 'en el desierto', 'en lo alto de la montaña'];     
-let when = ['esta mañana','hace 10 minutos','anoche','durante la clase de Jimena','la semana pasada.'];
+let who = ['Un elefante','Un alienigena','Mi compañero de clase','Mi vecino',];
+let what = ['lo pisoteó','lo comió','lo reventó','lo escupió', 'lo lanzó'];         
+let where =['en un helicoptero', 'en un lago', 'en el desierto', 'en la luna'];     
+let when = ['esta mañana','ahora','anoche','durante la clase de Jimena','la semana pasada.'];
 
-    for (var i = 0; i < who.length; i++){
+   
         let whoAleatorio = Math.floor(Math.random() * (who.length -1) +1);
         let whatAleatorio = Math.floor(Math.random() * (what.length -1) +1);
         let whereAleatorio = Math.floor(Math.random() * (where.length -1) +1);
-        let whenAleatorio = Math.floor(Math.random() * (when.length -1) +1);
+        let whenAleatorio = Math.floor(Math.random() * (when.length -1) +1)
 
-        let concatena = who[whoAleatorio].concat(" ", what[whatAleatorio], " ", where[whereAleatorio], " ",when[whenAleatorio]);
-        console.log(concatena);
+        let concatena = who[whoAleatorio].concat(" ", what[whatAleatorio], " ", where[whereAleatorio], " ", when[whenAleatorio]);
         document.getElementById("excuse").innerHTML = concatena;
-    }
+    
 
 }
+
+
 
 
 
